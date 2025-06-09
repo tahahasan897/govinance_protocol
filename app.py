9bd5dc34daa536b8044c798dda0359e833247eec
Taha Hasan <tahahasan897@gmail.com>
2025-06-09 16:14:38 -0400

    Updated website layout

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 from web3 import Web3
 import json
 import os
@@ -6,15 +6,16 @@ from dotenv import load_dotenv
 import requests
 
 # Always load environment variables from the repository root
-REPO_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+REPO_ROOT = os.path.dirname(os.path.abspath(__file__))
 ENV_PATH = os.path.join(REPO_ROOT, "necessities.env")
 load_dotenv(ENV_PATH)
 
 # Configuration
 RPC_URL = os.getenv("RPC_URL")
 CONTRACT_ADDRESS = os.getenv("CONTRACT_ADDRESS")
+MY_WALLET_ADDRESS = os.getenv("MY_WALLET_ADDRESS")
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 w3 = Web3(Web3.HTTPProvider(RPC_URL, request_kwargs={"timeout": 5}))
 
 # Load the ABI relative to this file so running from other directories works
@@ -33,7 +34,16 @@ contract = w3.eth.contract(
 @app.route("/")
 def index():
     # TODO:
-    return send_from_directory(os.path.join(REPO_ROOT, "templates"), "index.html")
+    return render_template("index.html")
+
+@app.route("/whitepaper", methods=["GET"])
+def whitepaper():
+    return render_template("whitepaper.html")
+
+@app.route("/fund", methods=["GET"])
+def fund():
+    return render_template("fund.html", my_wallet_address=MY_WALLET_ADDRESS)
+    
 
 if __name__ == "__main__":
     # Print out all registered routes to confirm /supply-data is active
